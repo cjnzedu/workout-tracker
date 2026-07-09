6da5978daa091c99d4ade37f6f005121d7d30068
Chimdi Nzedu <cjnzedu@gmail.com>
2026-07-09 14:50:30 -0400

    Added menu and feature to add/view workouts

--- workout-tracker.py
+++ workout-tracker.py
@@ -1,1 +1,34 @@
-print("Workout Tracker")
+workouts = []
+
+while True:
+    print("\nWorkout Tracker")
+    print("1. Add Workout")
+    print("2. View Workouts")
+    print("3. Quit")
+
+    choice = input("What do you want to do:  ")
+
+    if choice == "1":
+        print("Add Workouts Selected")
+        exercise = input("What Exercise do you want to add: ")
+        sets = int(input("Sets: "))
+        reps = int(input("Reps: "))
+        weight = int(input("Weight: "))
+
+        workout = {
+            "exercise": exercise,
+            "sets": sets,
+            "reps": reps,
+            "weight": weight
+        }
+        workouts.append(workout)
+        print("Workout Added!")
+
+    elif choice == "2":
+        print("View Workouts Selected")
+        print(workouts)
+    elif choice == "3":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid Choice!")
