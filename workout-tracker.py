1733c3e7d0077dfae80fdd8fb461c3d5badf84b2
Chimdi Nzedu <cjnzedu@gmail.com>
2026-07-11 12:05:44 -0400

    separated into functions and modified main

--- workout-tracker.py
+++ workout-tracker.py
@@ -1,38 +1,47 @@
-workouts = []
+def main():
+    workouts = []
+    while True:
+        show_menu()
+        choice = input("What do you want to do:  ")
+        if choice == "1":
+            add_workout(workouts)
+        elif choice == "2":
+            view_workouts(workouts)
+        elif choice == "3":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid Option")
 
-while True:
+def show_menu():
     print("\nWorkout Tracker")
     print("1. Add Workout")
     print("2. View Workouts")
     print("3. Quit")
+        
 
-    choice = input("What do you want to do:  ")
+def add_workout(workouts):
+    print("Add Workouts Selected")
+    exercise = input("What Exercise do you want to add: ")
+    sets = int(input("Sets: "))
+    reps = int(input("Reps: "))
+    weight = int(input("Weight: "))
 
-    if choice == "1":
-        print("Add Workouts Selected")
-        exercise = input("What Exercise do you want to add: ")
-        sets = int(input("Sets: "))
-        reps = int(input("Reps: "))
-        weight = int(input("Weight: "))
+    workout = {
+        "exercise": exercise,
+        "sets": sets,
+        "reps": reps,
+        "weight": weight
+    }
+    workouts.append(workout)
+    print("Workout Added!")
 
-        workout = {
-            "exercise": exercise,
-            "sets": sets,
-            "reps": reps,
-            "weight": weight
-        }
-        workouts.append(workout)
-        print("Workout Added!")
+def view_workouts(workouts):
+    print("View Workouts Selected")
+    if not workouts:
+        print("No workouts added yet...")
+        return
+    for index, workout in enumerate(workouts, start=1):
+        print(f"{index}. {workout['exercise']} {workout['sets']} x {workout['reps']} @{workout['weight']} lbs")
 
-    elif choice == "2":
-        print("View Workouts Selected")
-        if len(workouts) == 0:
-            print("No workouts added yet...")
-        for index, workout in enumerate(workouts, start=1):
-            print(f"{index}. {workout['exercise']} {workout['sets']} x {workout['reps']} @{workout['weight']} lbs")
-
-    elif choice == "3":
-        print("Goodbye!")
-        break
-    else:
-        print("Invalid Choice!")
+main()
